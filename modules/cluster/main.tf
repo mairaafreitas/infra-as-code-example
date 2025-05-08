92719ab1f3cb12158050d47596e99a4e67ead6f8
Maíra Oliveira de Freitas <maira.oliveira@loggi.com>
2025-05-08 08:30:38 -0300

    refactor: update cluster module to use launch templates and auto scaling group

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -7,31 +7,41 @@ data "aws_secretmanager_secret_version" "current" {
   secret_id = data.aws_secretmanager_secret.secret.id
 }
 
-resource "aws_instance" "instances" {
-  count                  = var.instance_count
-  ami                    = "ami-01cd4de4363ab6ee8"
+resource "aws_launch_template" "template" {
+  name = "${var.prefix}-template"
+  image_id               = "ami-01cd4de4363ab6ee8"
   instance_type          = "t2.micro"
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.security_group_ids
 
-  user_data = <<-EOF
+  user_data = base64encode(
+  <<-EOF
     #!/bin/bash
     DB_STRING="Server=${jsondecode(data.aws_secretmanager_secret_version.current.secret_string)["Host"]}; DB=${jsondecode(data.aws_secretmanager_secret_version.current.secret_string)["DB"]}; "
     echo $DB_STRING > test.txt
   EOF
+  )
 
-  tags = {
-    Name = "${var.prefix}-node-${count.index}"
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = var.security_group_ids
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.prefix}-node"
+    }
   }
 }
 
-resource "aws_eip" "eip" {
-  instance   = aws_instance.instance.id
-  depends_on = [ aws_internet_gateway.internet_gateway ]
-}
-
-resource "aws_ssm_paramenter" "ssm_parameter" {
-  name  = "vm_ip"
-  type  = "String"
-  value = aws_eip.eip.public_ip
+resource "aws_autoscaling_group" "autoscaling_group" {
+  name= "${var.prefix}-autoscaling-group"
+  desired_capacity     = 2
+  max_size             = 1
+  min_size             = 3
+  vpc_zone_identifier = var.subnet_ids
+  
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
 }
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "private_dns" {
-  value = module.cluster.private_dns
-}
-
-output "eip" {
-  value = aws_eip.eip.public_ip
-}
