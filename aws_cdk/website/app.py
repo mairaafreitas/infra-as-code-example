b7da1188d40da85ddf643e35eba906c3df3bd3d0
Maíra Oliveira de Freitas <maira.oliveira@loggi.com>
2025-05-16 14:55:41 -0300

    feat: add development and production website stacks

--- aws_cdk/website/app.py
+++ aws_cdk/website/app.py
@@ -5,6 +5,9 @@ from website.website_stack import WebsiteStack
 
 
 app = cdk.App()
-WebsiteStack(app, "WebsiteStack", "website-")
+WebsiteStack(app, "DevWebsiteStack", "dev-website-",
+             env=cdk.Environment(account="304851244341", region="us-west-2"))
+WebsiteStack(app, "ProdWebsiteStack", "prod-website-",
+             env=cdk.Environment(account="590183902781", region="us-west-2"))
 
 app.synth()
